98740d6e56a1f95ef9e80d3928ec01f899a6451a
Carlos Castro <carloscastrogames@gmail.com>
2021-06-09 19:49:10 -0300

    get with paraments

--- controllers/atendimentos.js
+++ controllers/atendimentos.js
@@ -1,8 +1,18 @@
 const Atendimento = require('../models/model_atendimentos')
 
 module.exports = app => {
-    app.get('/atendimentos', (req,res)=> res.send('você esta na rotas de atencimento'));
+    app.get('/atendimentos', (req,res)=> {
+      Atendimento.lista(res);
+    });
 
+    app.get('/atendimentos/:id',(req,res)=>{
+      const id = parseInt(req.params.id);
+
+      Atendimento.BuscarAtendimento(id, res);
+     
+
+ 
+    });
 
     app.post('/atendimentos', (req,res)=> {
       const atendimento = req.body;
--- models/model_atendimentos.js
+++ models/model_atendimentos.js
@@ -48,6 +48,32 @@ class ModelAtendimento{
     }
 
 
+    lista(res){
+        const sql = 'SELECT * FROM  Atendimentos'
+
+        conexao.query(sql,(erro, resultados) =>{
+            if(erro){
+                res.status(400).json(erro)
+            }else{
+                res.status(200).json(resultados)
+            }
+        })
+    }
+
+    BuscarAtendimento(id,res){
+        const sql = `SELECT * FROM Atendimentos WHERE id=${id}`
+        
+        conexao.query(sql,(erro,resultados) => {
+            const atendimento = resultados[0];
+            if(erro){
+                res.status(400).json(erro)
+            }else {
+                res.status(200).json(atendimento);
+            }
+
+        });
+    }
+
 }
 
 module.exports = new ModelAtendimento;
