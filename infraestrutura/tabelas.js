2032d82c4b3f9136aab2f46dab098651e0478890
Carlos Castro <carloscastrogames@gmail.com>
2021-06-09 19:18:43 -0300

    status cod

--- infraestrutura/tabelas.js
+++ infraestrutura/tabelas.js
@@ -14,7 +14,7 @@ class Tabelas {
             if (erro) {
                 console.log(erro)
             } else {
-                console.log('Tabela criada com sucesso')
+                console.log('Veriificando tabelas')
             }
         });
     }
--- models/model_atendimentos.js
+++ models/model_atendimentos.js
@@ -2,20 +2,21 @@ const moment = require('moment');
 const conexao = require('../infraestrutura/conexao');
 
 class ModelAtendimento{
-    adiciona(Atendimento){
+    adiciona(Atendimento, res){
            
             const dataCriacao = moment().format('YYYY-MM-DD HH:MM:SS');
          
-            const data =moment().format('YYYY-MM-DD HH:MM:SS');  //moment(Atendimento.data, formar:'DD/MM/YYYY').format('YYYY-MM-DD HH:mm:ss');
+            const data  =moment().format('YYYY-MM-DD HH:MM:SS');  //moment(Atendimento.data, formar:'DD/MM/YYYY').format('YYYY-MM-DD HH:mm:ss');
            
             const atendimentoDatado = {...Atendimento,dataCriacao, data}
 
             const sql = 'INSERT INTO atendimentos SET ?'
+
             conexao.query(sql, atendimentoDatado, (erro, resultados) => {
                 if(erro){
-                    console.log(erro)
+                    res.status(400).json(erro)
                 }else{
-                    console.log(resultados)
+                    res.status(201).json(resultados)
                 }
             });
     }
