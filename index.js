1c4903065880bd17441a8fbedc477625e1a44722
Carlos Castro <carloscastrogames@gmail.com>
2021-06-09 14:39:05 -0300

    function create tablet in mysql

--- index.js
+++ index.js
@@ -1,6 +1,8 @@
 
 const config = require('./config/config');
 const conexao = require('./infraestrutura/conexao');
+const tabelas = require('./infraestrutura/tabelas');
+
 
 
 //conectar ao banco
@@ -9,6 +11,8 @@ conexao.connect(erro =>{
         console.log('Erro na conexão do banco de dados' + erro.message);
     }else {
         console.log('Conectado com sucesso');
+
+        tabelas.init(conexao);
 
         //start app
         const app = config();
