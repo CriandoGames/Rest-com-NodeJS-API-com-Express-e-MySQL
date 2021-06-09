1a72f438395e9e98ba74be6d951197fb6feea587
Carlos Castro <carloscastrogames@gmail.com>
2021-06-09 16:26:35 -0300

    formart date now Work well

--- models/model_atendimentos.js
+++ models/model_atendimentos.js
@@ -1,10 +1,14 @@
+const moment = require('moment');
 const conexao = require('../infraestrutura/conexao');
 
 class ModelAtendimento{
     adiciona(Atendimento){
            
-            const dataCriacao = new Date();
-            const atendimentoDatado = {...Atendimento,dataCriacao}
+            const dataCriacao = moment().format('YYYY-MM-DD HH:MM:SS');
+         
+            const data =moment().format('YYYY-MM-DD HH:MM:SS');  //moment(Atendimento.data, formar:'DD/MM/YYYY').format('YYYY-MM-DD HH:mm:ss');
+           
+            const atendimentoDatado = {...Atendimento,dataCriacao, data}
 
             const sql = 'INSERT INTO atendimentos SET ?'
             conexao.query(sql, atendimentoDatado, (erro, resultados) => {
