8271ee9afff7901f78dd623cede1a9d05b1ec3e1
Carlos Castro <carloscastrogames@gmail.com>
2021-06-09 13:53:40 -0300

    install bodyParser
    just student

--- controllers/atendimentos.js
+++ controllers/atendimentos.js
@@ -1,3 +1,10 @@
 module.exports = app => {
     app.get('/atendimentos', (req,res)=> res.send('você esta na rotas de atencimento'));
+
+
+    app.post('/atendimentos', (req,res)=> {
+        console.log(req.body);
+        
+        res.send('metodo post')
+    });
 }
