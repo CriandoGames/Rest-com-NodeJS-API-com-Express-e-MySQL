ffa0bfc665e2cead7a6681a197ec941edeca1a3f
Carlos Castro <carloscastrogames@gmail.com>
2021-06-09 14:55:49 -0300

    post route Atendimento with json OK

--- controllers/atendimentos.js
+++ controllers/atendimentos.js
@@ -1,10 +1,13 @@
+const Atendimento = require('../models/model_atendimentos')
+
 module.exports = app => {
     app.get('/atendimentos', (req,res)=> res.send('você esta na rotas de atencimento'));
 
 
     app.post('/atendimentos', (req,res)=> {
-        console.log(req.body);
+      const atendimento = req.body;
         
+      Atendimento.adiciona(atendimento);
         res.send('metodo post')
     });
 }
---
+++ models/model_atendimentos.js
@@ -0,0 +1,19 @@
+const conexao = require('../infraestrutura/conexao');
+
+class ModelAtendimento{
+    adiciona(Atendimento){
+            const sql = 'INSERT INTO atendimentos SET ?'
+
+            conexao.query(sql, Atendimento, (erro, resultados) => {
+                if(erro){
+                    console.log(erro)
+                }else{
+                    console.log(resultados)
+                }
+            });
+    }
+
+
+}
+
+module.exports = new ModelAtendimento;
