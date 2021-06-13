1e227df45e0d69c25e85b24f85b28c4ed344740e
Carlos Castro <carloscastrogames@gmail.com>
2021-06-13 16:22:54 -0300

    usando lib path e enviando arquivo com a extensão

--- arquivos/upload.js
+++ arquivos/upload.js
@@ -1,11 +1,13 @@
 const fs = require('fs');
-
+const path = require('path');
 
 
 module.exports = (caminho,nomedoArquivo,callbackImagemCriada) =>{
 
+
+	const tipo = path.extname(caminho)
 	//criando novo caminho para imagem
-	const novoCaminho =`./assets/imagens/${nomedoArquivo}`
+	const novoCaminho =`./assets/imagens/${nomedoArquivo}${tipo}`
 
 	fs.createReadStream(caminho).pipe(
 		fs.createWriteStream(`./assets/imagens/${nomedoArquivo}`)
